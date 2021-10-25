555b112d7ee68198e8bee1f83e410ef98b287527
almaevs <alma_erika_v@hotmail.com>
2021-10-25 18:56:13 -0500

    cambios

--- public/js/logica.js
+++ public/js/logica.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:8080');
+var socket = io.connect('');
 var list = document.querySelector('#lista-users');
 var username = window.location.pathname.replace('/chat/', '');
 var clientes = [];
